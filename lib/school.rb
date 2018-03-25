2e5d15fc5cd979186ebf3d14e5a043f54d6b91f6
Greg Stimson <stimsong@yahoo.com>
2018-03-25 18:12:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade].includes?
+    if @roster[grade].include?
       @roster[grade] << name
     else
       @roster[grade] = []
